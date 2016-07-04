a493a54228464300c4ae697cf5b68d90c7f93a07
Ana Maria <anamaria.garcia@bq.com>
2016-07-04 09:04:15 +0200

    [SILKROAD-2746] Allow search in certain fields
    Change-Id: Id1d877f062e3dff08bdcf26b0a3915f7d26075d2

--- queries-api/src/test/java/com/bq/corbel/lib/queries/parser/CustomSearchParserTest.java
+++ queries-api/src/test/java/com/bq/corbel/lib/queries/parser/CustomSearchParserTest.java
@@ -10,6 +10,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.bq.corbel.lib.queries.exception.MalformedJsonQueryException;
 import com.bq.corbel.lib.queries.request.Search;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author Rubén Carrasco
  *
@@ -24,43 +27,99 @@ public class CustomSearchParserTest {
     }
 
     @Test
-    public void testValidString() throws MalformedJsonQueryException {
-        String searchString = "text";
+    public void testParseValidStringWithoutTextField() throws MalformedJsonQueryException {
+        String searchString = "batman";
         Search search = parser.parse(searchString, false);
-        assertThat(search.getText().get()).isEqualTo("text");
+
+        assertThat(search.getText().get()).isEqualTo("batman");
+        assertThat(search.getTemplate().isPresent()).isEqualTo(false);
+        assertThat(search.getParams().isPresent()).isEqualTo(false);
+        assertThat(search.getFields().isPresent()).isEqualTo(false);
         assertThat(search.indexFieldsOnly()).isFalse();
     }
 
     @Test
-    public void testValidObject() throws MalformedJsonQueryException {
+    public void testParseValidStringWithTextField() throws MalformedJsonQueryException {
+        String searchString = "{\"text\": \"batman\"}";
+        Search search = parser.parse(searchString, false);
+
+        assertThat(search.getText().get()).isEqualTo("batman");
+        assertThat(search.getTemplate().isPresent()).isEqualTo(false);
+        assertThat(search.getParams().isPresent()).isEqualTo(false);
+        assertThat(search.getFields().isPresent()).isEqualTo(false);
+        assertThat(search.indexFieldsOnly()).isFalse();
+    }
+
+    @Test
+    public void testParseValidStringWithFields() throws MalformedJsonQueryException {
+        String searchString = "{\"text\": \"batman\", \"fields\": [\"title\", \"authors\"]}";
+        Search search = parser.parse(searchString, false);
+
+        assertThat(search.getText().get()).isEqualTo("batman");
+
+        List<String> fields = search.getFields().get();
+        assertThat(fields).isNotNull();
+        assertThat(fields.get(0)).isEqualTo("title");
+        assertThat(fields.get(1)).isEqualTo("authors");
+
+        assertThat(search.getTemplate().isPresent()).isEqualTo(false);
+        assertThat(search.getParams().isPresent()).isEqualTo(false);
+        assertThat(search.indexFieldsOnly()).isFalse();
+    }
+
+    @Test
+    public void testParseWrongFieldsNameWithTextReturnsFullTextSearch() throws MalformedJsonQueryException {
+        // It should be fields, not field
+        String searchString = "{\"text\": \"batman\", \"field\": [\"title\", \"authors\"]}";
+        Search search = parser.parse(searchString, false);
+
+        assertThat(search.getText().get()).isEqualTo("batman");
+        assertThat(search.getTemplate().isPresent()).isEqualTo(false);
+        assertThat(search.getParams().isPresent()).isEqualTo(false);
+        assertThat(search.getFields().isPresent()).isEqualTo(false);
+        assertThat(search.indexFieldsOnly()).isFalse();
+    }
+
+    @Test
+    public void testParseWithValidTemplate() throws MalformedJsonQueryException {
         String searchString = "{\"templateName\": \"name\", \"params\": {}}";
         Search search = parser.parse(searchString, true);
+
         assertThat(search.getTemplate().get()).isEqualTo("name");
         assertThat(search.indexFieldsOnly()).isTrue();
+        assertThat(search.getParams().get()).isEmpty();
+        assertThat(search.getFields().isPresent()).isEqualTo(false);
+        assertThat(search.getText().isPresent()).isEqualTo(false);
     }
 
     @Test(expected = MalformedJsonQueryException.class)
-    public void testInvalidName() throws MalformedJsonQueryException {
+    public void testParseInvalidTemplateName() throws MalformedJsonQueryException {
         String searchString = "{\"templateName\": {}, \"params\": {}}";
         parser.parse(searchString, true);
     }
 
     @Test(expected = MalformedJsonQueryException.class)
-    public void testInvalidParams() throws MalformedJsonQueryException {
+    public void testParseInvalidTemplateParams() throws MalformedJsonQueryException {
         String searchString = "{\"templateName\": \"name\", \"params\": []}";
         parser.parse(searchString, true);
     }
 
     @Test(expected = MalformedJsonQueryException.class)
-    public void testTemplateSerachWithoutName() throws MalformedJsonQueryException {
+    public void testTemplateSearchWithoutName() throws MalformedJsonQueryException {
         String searchString = "{\"params\": {}}";
         parser.parse(searchString, false);
     }
 
     @Test(expected = MalformedJsonQueryException.class)
-    public void testTemplateSerachWithoutParams() throws MalformedJsonQueryException {
+    public void testTemplateSearchWithoutParams() throws MalformedJsonQueryException {
         String searchString = "{\"templateName\": \"name\"}";
         parser.parse(searchString, false);
+    }
+
+    @Test(expected = MalformedJsonQueryException.class)
+    public void testParseFieldsWithTextString() throws MalformedJsonQueryException {
+        String searchString = "{\"fields\": [\"title\", \"authors\"]}";
+        Search search = parser.parse(searchString, false);
     }
 
 }
